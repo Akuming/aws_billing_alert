543674cbabe3ec2beb831714cacc486f688671e7
Akuming <akwasiakumingamoah@gmail.com>
2023-06-15 09:08:21 +0000

    Publish message to emails subscribed to the topic

--- lambda_function.py
+++ lambda_function.py
@@ -2,6 +2,24 @@ import boto3
 import os
 import json
 from datetime import datetime, timedelta
+from botocore.exceptions import ClientError
+
+
+def send_sns_notification(daily_cost, monthly_cost):
+    sns = boto3.client('sns')
+    topic_arn = os.environ['SNS_TOPIC_ARN']
+
+    message = f"Today's AWS bill: ${daily_cost:.2f}\nTotal bill for the month: ${monthly_cost:.2f}"
+    try:
+        response = sns.publish(
+            TopicArn=topic_arn,
+            Message=message,
+            Subject='AWS Billing Update'
+        )
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+    else:
+        print(f"Message sent to SNS topic {topic_arn}")
 
 def lambda_handler(event, context):
     ce = boto3.client('ce')
@@ -28,6 +46,8 @@ def lambda_handler(event, context):
     print("The daily cost is ", daily_cost)
     monthly_cost = sum(float(res['Metrics']['UnblendedCost']['Amount']) for day in response['ResultsByTime'] for res in day['Groups'])
     print("The monthly cost is ", monthly_cost)
+    
+    send_sns_notification(daily_cost, monthly_cost)
 
     return {
         'statusCode': 200,
